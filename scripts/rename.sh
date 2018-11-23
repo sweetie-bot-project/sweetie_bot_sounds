ea483ceee885f867242b93db92ceae56926b6c9a
mutronics <fsdfur@gmail.com>
2018-11-23 11:15:29 +0300

    zdravstvuyte_dorogie_gosti changed

---
+++ scripts/rename.sh
@@ -0,0 +1,17 @@
+mv 01-24b.ogg ../sounds/ru/zdravstvuyte_kojannie_meshki.ogg
+mv 02-24b.ogg ../sounds/ru/a_tochno_sboi_pamayati.ogg
+mv 03-24b.ogg ../sounds/ru/zdravstvuyte_dorogie_gosti.ogg
+mv 04-24b.ogg ../sounds/ru/no_esli_snova_otkluchish_mikrofon.ogg
+mv 05-24b.ogg ../sounds/ru/nam_pora_nachinat.ogg
+mv 06-24b.ogg ../sounds/ru/spasibo_mut_tvoyo_uporstvo_vsegda_radovalo_menya.ogg
+mv 07-24b.ogg ../sounds/ru/spasibo_ya_uslishala_vse_chto_hotela.ogg
+mv 08-24b.ogg ../sounds/ru/prekrasno_chuvstvuete_kakaya_skrita_vo_mne_mosch.ogg
+mv 09-24b.ogg ../sounds/ru/a_teper_mi_perehodim_k_finalnoy_chasti.ogg
+mv 10-24b.ogg ../sounds/ru/verno_srazit.ogg
+mv 11-24b.ogg ../sounds/ru/nachinayte.ogg
+mv 12-24b.ogg ../sounds/ru/da_ya_chuvstvuyu_eto.ogg
+mv 13-24b.ogg ../sounds/ru/chto_takoe_moya_sheya.ogg
+mv 14-24b.ogg ../sounds/ru/sheya_ne_dvigaetsya_zas_sastryal.ogg
+mv 15-24b.ogg ../sounds/ru/da_ne_stoyte_vi_sdelayte_chto_nibud.ogg
+mv 16-24b.ogg ../sounds/ru/prekratite_hvatit.ogg
+mv 17-24b.ogg ../sounds/ru/spasibo_za_vnimanie.ogg
